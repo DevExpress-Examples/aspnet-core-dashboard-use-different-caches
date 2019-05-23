9df1d2f7c6ca2a71393f569e9de69488c53c8153
DevExpressExampleBot <fakeEmail>
2019-05-23 10:53:19 +0300

    Source auto update [skip ci]

---
+++ VB/AspNetCoreDashboardUseDifferentCaches/HomeController.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports Microsoft.AspNetCore.Mvc
+
+' For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+Namespace AspNetCoreDashboardUseDifferentCaches
+	Public Class HomeController
+		Inherits Controller
+
+		' GET: /<controller>/
+		Public Function Index() As IActionResult
+			Return View()
+		End Function
+		Public Function Refresh(<FromServices> ByVal cacheManager As CacheManager) As IActionResult
+			cacheManager.ResetCache()
+			Return Ok()
+		End Function
+	End Class
+End Namespace
