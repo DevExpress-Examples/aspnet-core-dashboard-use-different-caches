081f6f1ec1fbc89062f8af976c88dcd13d835c96
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2022-07-14 16:37:45 +0300

    Update HomeController.cs

--- CS/AspNetCoreDashboardUseDifferentCaches/Controllers/HomeController.cs
+++ CS/AspNetCoreDashboardUseDifferentCaches/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace AspNetCoreDashboardUseDifferentCaches
-{
-    public class HomeController: Controller
-    {
+namespace AspNetCoreDashboardUseDifferentCaches {
+    public class HomeController: Controller {
         // GET: /<controller>/
         public IActionResult Index() {
             return View();
